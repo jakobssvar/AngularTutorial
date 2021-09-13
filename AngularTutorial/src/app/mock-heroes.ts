df5c2313016fc7b4c6eed6a8c5a757012e350a31
svarsstad <sandsvarstad@gmail.com>
2021-09-13 14:30:54 +0200

    minor modifications

--- AngularTutorial/src/app/mock-heroes.ts
+++ AngularTutorial/src/app/mock-heroes.ts
@@ -9,7 +9,7 @@ export const HEROES: Hero[] = [
   { id: 5, name: 'Fredrick', description: 'the black butler' },
   { id: 6, name: 'George', description: 'the king' },
   { id: 7, name: 'Helen', description: 'of Troy' },
-  { id: 8, name: 'Ingrid', description: 'the fearociously obtuse' },
+  { id: 8, name: 'Ingrid', description: 'the ferociously obtuse' },
   { id: 9, name: 'Jakob', description: 'the only one' },
   { id: 10, name: 'Kaia', description: 'the second only one' },
   { id: 11, name: 'Lars', description: 'the not-so-great one' },
