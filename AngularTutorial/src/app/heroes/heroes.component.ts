138357d1d22f3ad1d00ae8a4aff1a4fbe96196ef
svarsstad <sandsvarstad@gmail.com>
2021-09-13 14:24:44 +0200

    step 2 added a list

--- AngularTutorial/src/app/heroes/heroes.component.ts
+++ AngularTutorial/src/app/heroes/heroes.component.ts
@@ -1,36 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
+import { HEROES } from '../mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
+
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: "Jakob",
-    description: "the king of kings"
-  };
+
+  heroes = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
-
-  changeName(): void {
-    this.hero.name = "Jakob";
-
-  }
-
-  
-
   ngOnInit(): void {
   }
-  ngOnChange(): void {
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
 
   }
-
-  ngOnDestroy(): void {
-
-  }
-
 }
+
+
+
--- AngularTutorial/src/app/mock-heroes.ts
+++ AngularTutorial/src/app/mock-heroes.ts
@@ -1,2 +1,20 @@
-import { Hero } from './hero'
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id:0, name:'Anne', description:'the great one'},
+  { id: 1, name: 'Ben', description: 'the old one' },
+  { id: 2, name: 'Citara', description: 'the sour face' },
+  { id: 3, name: 'Dagar', description: 'the fearsome' },
+  { id: 4, name: 'Erik', description: 'the viking' },
+  { id: 5, name: 'Fredrick', description: 'the black butler' },
+  { id: 6, name: 'George', description: 'the king' },
+  { id: 7, name: 'Helen', description: 'of Troy' },
+  { id: 8, name: 'Ingrid', description: 'the fearociously obtuse' },
+  { id: 9, name: 'Jakob', description: 'the only one' },
+  { id: 10, name: 'Kaia', description: 'the second only one' },
+  { id: 11, name: 'Lars', description: 'the not-so-great one' },
+  { id: 12, name: 'Morten', description: 'not-the-Harket one' }
+
+
+]
 
